fb0fff471b628aaa326a694a7422d17cf5116906
miterfrants <miterfrants@gmail.com>
2012-07-24 01:44:38 +0800

    Initial Test

--- guidebird/AppDelegate.h
+++ guidebird/AppDelegate.h
@@ -5,7 +5,6 @@
 //  Created by 柏翔 黃 on 12/7/24.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
